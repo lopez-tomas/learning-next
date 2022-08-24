b9bc7421955f46ab9835f6a9dbad76bc721f4f36
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-08-24 20:19:27 -0300

    [FEAT]: Footer component created.

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.sass';
+
+const Footer = () => {
+  return (
+    <footer className={styles.Footer}>
+      This is the footer
+    </footer>
+  )
+}
+
+export default Footer;
