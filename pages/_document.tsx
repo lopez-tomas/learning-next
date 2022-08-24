ac1d53a6b226960c94cb903707d29ed6de3a4655
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-08-24 15:01:02 -0300

    [FEAT]: extending Document in NextJS.

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* favicon */}
+        {/* webfont */}
+        {/* external styles */}
+        {/* external scripts/js */}
+      </Head>
+      <body className="my-body-class">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
