117052c70a7b8c7149464d2d3a3305b27a95bd00
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-08-24 14:52:05 -0300

    [FEAT]: using fetch to get product data. ProductItem component imported & used.

--- pages/products/[productId].tsx
+++ pages/products/[productId].tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import ProductItem from '@/components/Products/ProductItem';
 
-const Character = () => {
+const Product = () => {
+  const [product, setProduct] = useState<TProduct>();
+
   const {
     query: { productId }
   } = useRouter();
 
+  useEffect(() => {
+    window.fetch(`/api/products/${productId}`)
+      .then(response =>
+        response.json()
+      )
+      .then(data =>
+        setProduct(data)
+      )
+      .catch(error => {
+        console.error(error)
+      });
+  }, [productId]);
+
+  if (!product) {
+    return null;
+  }
+
   return (
-    <div>
+    <div className='Product'>
       Product with ID: {productId}
+      <ProductItem data={product} />
     </div>
   )
 }
 
-export default Character;
+export default Product;
