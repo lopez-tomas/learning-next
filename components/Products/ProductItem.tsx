f0e25adc986840017800476a423c4bdc84338816
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-08-24 14:50:20 -0300

    [FEAT]: showing product data.

--- components/Products/ProductItem.tsx
+++ components/Products/ProductItem.tsx
@@ -1,15 +1,21 @@
-// import React from 'react'
+//import React from 'react'
 
 type Props = {
-  key: TProductId;
   data: TProduct;
 }
 
 const ProductItem = ({ data }: Props) => {
   return (
-    <li className="ProductItem">
-      {data.name}
-    </li>
+    <div className="ProductItem">
+      <img src={data.image} />
+      <h2>{data.name}</h2>
+      <p>{data.price}</p>
+      <p>sku: {data.sku}</p>
+      <p>{data.attributes['description']}</p>
+      <p>Hardiness: {data.attributes['hardiness']}</p>
+      <p>Shape: {data.attributes['shape']}</p>
+      <p>Taste: {data.attributes['taste']}</p>
+    </div>
   )
 }
 
