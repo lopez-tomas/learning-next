30ff305d4387712d1ec5366da58688057e6ac15a
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-09-20 00:08:55 -0300

    [FEAT]: using getServerSideProps to obtain all characters. Data is sent to CharacterList component through props.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
 import CharacterList from '@components/Characters/CharacterList';
+import useGetAllCharacters from '@hooks/useGetAllCharacters';
 
-const Home = () => {
+interface Props {
+  info: TInfo;
+  characters: TCharacter[];
+}
+
+const Home: React.FC<Props> = ({ info, characters }) => {
   return (
-    <div className='Home'>
+    <main className='Home'>
       <h1 className='Home-title'>HOME</h1>
-      <CharacterList />
-    </div>
+      <div className='Characters'>
+        <CharacterList info={info} characters={characters} />
+      </div>
+    </main>
   )
+}
+
+export async function getServerSideProps() {
+  const data = await useGetAllCharacters();
+
+  return {
+    props: {
+      info: data.info,
+      characters: data.results
+    }
+  }
 }
 
 export default Home;
