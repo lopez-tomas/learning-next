7a796fe85eef8e8bae218350ed146757e516734b
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-09-22 03:40:23 -0300

    [FEAT]: Footer component updated. Links to GitHub and Instagram added.

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+// import React from 'react';
+import Image from 'next/image'
 import styles from './Footer.module.sass';
 
 const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      This is the footer
+      <p className={styles['Footer-author']}>
+        Made with ❤ by <a className={styles['Footer-author--link']} href="https://github.com/lopez-tomas" target="__blank">Tomás López</a>
+      </p>
+      <div className={styles['Footer-links']}>
+        <a href="https://github.com/lopez-tomas" target="__blank" rel="noopner nofollow" aria-label="Check out my GitHub">
+          <Image src='/github.svg' alt='GitHub' width="30" height="30" />
+        </a>
+        <a href="https://instagram.com/tomas.dev" target="__blank" rel="noopner nofollow" aria-label="Check out my GitHub">
+          <Image src='/instagram.svg' alt='Instagram' width="30" height="30" />
+        </a>
+      </div>
     </footer>
   )
 }
