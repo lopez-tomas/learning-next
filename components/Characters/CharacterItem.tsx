0420ea65a4cf576a774165b9a4d2381282b0ab53
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-09-27 16:57:04 -0300

    [FEAT]: importing & using statusColor() & capitalizeFirstLetters() functions from /utils folder. Using next/link for Character name and Character image.

--- components/Characters/CharacterItem.tsx
+++ components/Characters/CharacterItem.tsx
@@ -1,23 +1,24 @@
 //import React from 'react'
-// import Link from 'next/link';
-
-const capitalizeFirstLetter = (string: string) => string == 'unknown' ? string.charAt(0).toUpperCase() + string.slice(1) : string;
+import Link from 'next/link';
+import { capitalizeFirstLetter, statusColor } from 'utils';
 
 interface Props {
   data: TCharacter;
 }
 
 const CharacterItem: React.FC<Props> = ({ data }) => {
-  const statusColor = data.status == 'Alive' ? '#55cc44' : data.status == 'Dead' ? '#d63d2e' : 'yellow';
+  // const statusColor = data.status == 'Alive' ? '#55cc44' : data.status == 'Dead' ? '#d63d2e' : 'yellow';
 
   return (
     <article className='CharacterItem'>
-      <img className='CharacterItem-image' src={data.image} alt={data.name} />
+      <Link href={`/character/${encodeURIComponent(data.id)}`} passHref>
+        <img className='CharacterItem-image' src={data.image} alt={data.name} />
+      </Link>
 
       <div className='CharacterItem-data'>
-        {/* <Link href={`/characters/${encodeURIComponent(data.id)}`} passHref> */}
-        <h2 className='CharacterItem-data--title'>{data.name}</h2>
-        {/* </Link> */}
+        <Link href={`/character/${encodeURIComponent(data.id)}`} passHref>
+          <h2 className='CharacterItem-data--title'>{data.name}</h2>
+        </Link>
 
         {data.type
           ?
@@ -45,7 +46,7 @@ const CharacterItem: React.FC<Props> = ({ data }) => {
 
       <style jsx>{`
         .CharacterItem-data--status span {
-          background-color: ${statusColor};
+          background-color: ${statusColor(data.status)};
         }
       `}</style>
     </article>
