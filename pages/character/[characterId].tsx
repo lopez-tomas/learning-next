6d5d581330e7561f439d6ff1eef9ab8b841eef18
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-09-27 17:02:30 -0300

    [FEAT]: new [characterId].tsx page. It's not finished yet. Styles added too.

--- pages/character/[characterId].tsx
+++ pages/character/[characterId].tsx
@@ -1,22 +1,72 @@
-//import React from 'react';
-import CharacterDetail from '@components/Characters/CharacterDetail';
+import { useState } from 'react';
 import useGetAllCharactersId from '@hooks/useGetAllCharactersId';
 import useGetCharacter from '@hooks/useGetCharacter';
+import useGetAllEpisodes from '@hooks/useGetAllEpisodes';
+
+import { statusColor } from 'utils';
+
+import styles from './Character.module.sass';
 
 interface Props {
   character: TCharacter;
+  episodes: TEpisode[];
 }
 
-const Character = ({ character }: Props) => {
+const Character = ({ character, episodes }: Props) => {
   if (!character) {
     return null;
   }
 
   return (
-    <div className='Product'>
-      Product with ID: {character.id}
-      <CharacterDetail data={character} />
-    </div>
+    <main className={styles.Character}>
+      <div className={styles['Info']}>
+        <div className={styles['Info__left']}>
+          <h2 className={styles['Info__left--name']}>{character.name}</h2>
+          <img className={styles['Info__left--img']} src={character.image} />
+        </div>
+
+        <div className={styles['Info__right']}>
+          <p><span className={styles['Info__right--status']}></span>Status: {character.status}</p>
+          {character.origin.name}<br/>
+          {character.gender}<br/>
+          {character.species} {character.type ? ` - ${character.type}` : ''}<br/>
+        </div>
+      </div>
+
+      <div className={styles['Episodes']}>
+        <h3 className={styles['Episodes__title']}>Episodes</h3>
+
+        <table className={styles['Episodes__table']}>
+          <thead>
+            <tr>
+              <th className={styles['Episodes__table--episode-th']}>Episode</th>
+              <th className={styles['Episodes__table--name-th']}>Name</th>
+              <th className={styles['Episodes__table--date-th']}>Air date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {(episodes).map((episode) => (
+              <tr key={episode.id}>
+                <td className={styles['Episodes__table--episode-td']}>{episode.episode}</td>
+                <td className={styles['Episodes__table--name-td']}>{episode.name}</td>
+                <td className={styles['Episodes__table--date-td']}>{episode.air_date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <style jsx>{`
+        .Info__right--status {
+          display: inline-block;
+          height: 10px;
+          width: 10px;
+          margin: 0 .5rem;
+          background-color: ${statusColor(character.status)};
+          border-radius: 50%;
+        }
+      `}</style>
+    </main>
   )
 }
 
@@ -33,11 +83,21 @@ interface StaticProps {
 }
 
 export async function getStaticProps({ params }: StaticProps) {
-  const character = await useGetCharacter(params.characterId);
+  const character: TCharacter = await useGetCharacter(params.characterId);
+
+  const episodes: TCharacter['episode'] = [];
+  character.episode.map(episode => {
+    const episodeId = episode.split('/').slice(-1)[0];
+    episodes.push(episodeId);
+  })
+
+  const query: string = episodes.join();
+  const episodesInfo = await useGetAllEpisodes(undefined, `/[${query}]`);
 
   return {
     props: {
       character,
+      episodes: episodesInfo
     }
   }
 }
