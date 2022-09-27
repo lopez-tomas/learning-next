295a03a08505068e7f3a1f3f2697277fb0715029
Tomás López (Frontend Developer) <lopeztomasenrique98@gmail.com>
2022-09-26 22:04:46 -0300

    [FEAT]: I'm not using 'CharacterList' component anymore. Now, all is managed through pages/index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
-import CharacterList from '@components/Characters/CharacterList';
+import { useEffect, useState } from 'react'
 import useGetAllCharacters from '@hooks/useGetAllCharacters';
+
+import Pagination from '@components/Pagination/Pagination';
+import CharacterItem from '@components/Characters/CharacterItem';
+import { Search } from '@components/Search/Search';
 
 interface Props {
   info: TInfo;
@@ -7,11 +11,58 @@ interface Props {
 }
 
 const Home: React.FC<Props> = ({ info, characters }) => {
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(false);
+  const [previous, setPrevious] = useState<Page>(info.prev);
+  const [next, setNext] = useState<Page>(info.next);
+  const [currentCharacters, setCurrentCharacters] = useState<TCharacter[]>(characters);
+  const API_URL = 'https://rickandmortyapi.com/api/character';
+
+  const fetchCharacters = async (url: Page, search?: string) => {
+    try {
+      const data = await useGetAllCharacters(url as string, search);
+      setCurrentCharacters(data.results);
+      setPrevious(data.info.prev);
+      setNext(data.info.next);
+      setError(false);
+    } catch (err) {
+      setError(true);
+    }
+  }
+
+  const onPrev = () => {
+    fetchCharacters(previous);
+  }
+
+  const onNext = () => {
+    fetchCharacters(next);
+    window.scrollTo(0,0);
+  }
+
+  useEffect(() => {
+    fetchCharacters(API_URL, `/?name=${search}`);
+  }, [search]);
+
   return (
     <main className='Home'>
-      <div className='Characters'>
-        <CharacterList info={info} characters={characters} />
-      </div>
+      <Search search={search} setSearch={setSearch} />
+
+      {!error &&
+        <div className='Characters'>
+          <Pagination prev={previous} next={next} onPrev={onPrev} onNext={onNext} />
+          <div className='CharacterList'>
+            {currentCharacters.map(character => (
+              <CharacterItem key={character.id} data={character} />
+              ))}
+          </div>
+          <Pagination prev={previous} next={next} onPrev={onPrev} onNext={onNext} />
+        </div>
+      }
+      {error &&
+        <div className='Error'>
+          <h1 className='Characters--error'>No characters found :(</h1>
+        </div>
+      }
     </main>
   )
 }
